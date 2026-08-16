99c637bb44d0b9fe2b425fa73035a12960f7604f
vodoleikin <vodoleikin@yandex.ru>
2026-08-16 13:05:03 +0300

    Added users-app and posts-app

--- social_mini_project/social_mini_project/settings.py
+++ social_mini_project/social_mini_project/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'posts',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
