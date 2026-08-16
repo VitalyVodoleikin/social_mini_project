edaee1737de72265bfbb72a211a8bf4af325c649
vodoleikin <vodoleikin@yandex.ru>
2026-08-16 17:23:18 +0300

    Some corrects in some templates and added 404.html

--- social_mini_project/social_mini_project/urls.py
+++ social_mini_project/social_mini_project/urls.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf.urls import handler404
 from django.urls import path, include
+
+
+handler404 = 'django.views.defaults.page_not_found'
 
 urlpatterns = [
     path("admin/", admin.site.urls),
